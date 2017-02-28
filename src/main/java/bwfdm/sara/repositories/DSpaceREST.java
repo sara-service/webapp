0921b34b74bdc792ccadbe902d38b6739c343bcb
Volodymyr Kushnarenko <volodymyr.kushnarenko@uni-ulm.de>
2017-02-28 20:36:23 +0100

    copied develop to master

---
+++ src/main/java/bwfdm/sara/repositories/DSpaceREST.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bwfdm.sara.repositories;
+
+import bwfdm.sara.metadata.MetadataCollection;
+
+/**
+ * Interface for the typical DSpace Publication Repository.
+ * 
+ * Extends PublicationRepository-Intrface
+ * 
+ * @author vk
+ */
+public interface DSpaceREST {
+    
+    public boolean isRestEnable();
+    public boolean login(String email, String password);
+    public boolean logout();
+    public String getToken();
+    
+    public String createCommunity(String communityName, String parentCommunityID);
+    public String deleteCommunity(String communityID);
+    public boolean updateCommunity(String communityName, String communityID);
+    
+    public String getObjectProperties(String objectName, String objectID);
+    
+    public String createCollection(String collectionName, String parentCommunityID);
+    public String deleteCollection(String collectionID);
+    public boolean updateCollection(String collectionName, String collectionID);
+    
+    public String createItem(String itemName, String itemTitle, String collectionID);
+    public boolean deleteItem(String itemID);
+    public boolean delteItemInCollection(String collectionID, String itemID);
+    
+    public boolean itemAddMetadata(String itemID, MetadataCollection collectionWithMetadata);
+    public boolean itemUpdateMetadata(String itemID, String metadataEntry);
+    public String itemAddBitstream(String itemID, String bitstreamToAdd);
+    public boolean itemDeleteBitstream(String itemID, String bitstreamToDelete);
+    
+    public boolean downloadBitstream(String bitstreamID, String filenameToSave);
+    
+    
+    
+
+}
