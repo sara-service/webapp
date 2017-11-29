59f85ab12ab0aae26f16c589d69bbfece05ad79e
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2017-11-29 13:24:11 +0100

    fixing permission script: postgresql 9.5+initdb.sh makes runnable sara server again

--- permissions.sql
+++ permissions.sql
@@ -1,14 +1,14 @@
 -- permission config for database tables in PostgreSQL.
 
--- database setup for PostgreSQL:
--- sudo mkdir -p /var/lib/tomcat7/lib
--- sudo mv postgresql-42.1.1.jar /var/lib/tomcat7/lib
--- sudo -u postgres createuser -D -I -S sara
--- sudo -u postgres createdb sara
--- sudo -u postgres psql sara -c "alter user sara password 'secret'"
--- sudo -u postgres psql sara -f schema.sql
--- sudo -u postgres psql sara -f permissions.sql
+-- rule of thumb: 
+--  all tables granted select only should reside
+--  all tables granted select, insert, update, delete should eventually be moved into publication sara DB!
 
--- frontend only needs CRUD
 grant select, insert, update, delete on fe_temp_metadata to test;
 grant select, insert, update, delete on fe_temp_actions to test;
+grant select, insert, update, delete on fe_temp_licenses to test;
+grant select, insert, update, delete on fe_supported_licenses to test;
+grant select on fe_gitrepos to test;
+grant select on fe_gitrepo_params to test;
+grant select on fe_archives to test;
+grant select on fe_archive_params to test;
