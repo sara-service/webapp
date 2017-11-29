29073f3ef0e6319e41d4bd829bc8b6daf9cb954a
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2017-11-29 14:51:54 +0100

    initializing integration of sara db

---
+++ src/main/java/bwfdm/sara/publication/db/RepositoryDAO.java
@@ -0,0 +1,41 @@
+package bwfdm.sara.publication.db;
+
+public class RepositoryDAO {
+	
+	public final String name;
+    public final String URL;
+    public final String query_API_endpoint;  // REST query API endpoint
+    public final String query_user;          // REST user name (queries only, no submissions)
+    public final String query_pwd;           // ...corresponding pwd/key
+    public final String submit_API_endpoint; // SWORD API endpoint
+    public final String submit_user;         // SWORD user name with submission rights
+    public final String submit_pwd;          // ...corresponding pwd/key
+    public final String contactEMail;        // an email address to contact the repository
+    
+    // optionally
+	public final String version;             // Show version info for compatibility.
+	public final String default_collection;         // submit into an default collection.
+
+    public RepositoryDAO(
+    		String n,
+    		String v,
+    		String u,
+    		String quAPIEP,
+    		String quUser,
+    		String quPwd,
+    		String suAPIEP,
+    		String suUser,
+    		String suPwd,
+    		String mailaddr,
+    		String defColl
+    		) {
+    	name = n; URL = u;
+    	query_API_endpoint = quAPIEP; query_user = quUser; query_pwd = quPwd;
+    	submit_API_endpoint = suAPIEP; submit_user = quUser; submit_pwd = quPwd;
+    	contactEMail = mailaddr;
+    	version = v; default_collection = defColl;
+    }
+
+    
+
+}
