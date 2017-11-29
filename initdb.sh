a247de8710fbc091dc4328f3153b935493866110
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2017-11-29 09:43:19 +0100

    fix it for my ubuntu machine and postgres 9.5

--- initdb.sh
+++ initdb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+# requires on ubuntu 16.10: apt-get install pgcrypto postgresql-9.5
+
 git submodule update saradb
 cat saradb/create-sara-db.sql | sed 's/sara/test/g' > schema-pub.sql
-rm -rf $TD
 
-#sudo /etc/init.d/postgres-xc restart
-sudo -u postgres-xc psql postgres -f schema-pub.sql
-sudo -u postgres-xc psql test -c "alter user test password 'test'"
-sudo -u postgres-xc psql test -f schema.sql
-sudo -u postgres-xc psql test -f permissions.sql
+sudo -u postgres psql postgres -f schema-pub.sql
+sudo -u postgres psql test -c "alter user test password 'test'"
+sudo -u postgres psql test -f schema.sql
+sudo -u postgres psql test -f permissions.sql
