96c556f424fa234aa1e84243d823cdb53466b11b
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2017-12-05 14:31:03 +0100

    refining db

--- src/main/java/bwfdm/sara/publication/db/RepositoryDAO.java
+++ src/main/java/bwfdm/sara/publication/db/RepositoryDAO.java
@@ -15,6 +15,7 @@ public class RepositoryDAO {
     public final String submit_API_endpoint; // SWORD API endpoint
     public final String submit_user;         // SWORD user name with submission rights
     public final String submit_pwd;          // ...corresponding pwd/key
+    public final String logo_base64;         // logo image encoded in base 64 for immediate use in <img> elements
     public final String contactEMail;        // an email address to contact the repository
     
     // optionally
@@ -23,8 +24,7 @@ public class RepositoryDAO {
 
     public RepositoryDAO(
     		UUID id,
-    		String n,
-    		String v,
+    		String n,    		
     		String u,
     		String quAPIEP,
     		String quUser,
@@ -32,14 +32,16 @@ public class RepositoryDAO {
     		String suAPIEP,
     		String suUser,
     		String suPwd,
+    		String v,
     		String mailaddr,
+    		String logo,
     		String defColl
     		) {
     	uuid = id; name = n; URL = u;
     	query_API_endpoint = quAPIEP; query_user = quUser; query_pwd = quPwd;
     	submit_API_endpoint = suAPIEP; submit_user = quUser; submit_pwd = quPwd;
     	contactEMail = mailaddr;
-    	version = v; default_collection = defColl;
+    	version = v; logo_base64 = logo;default_collection = defColl;
     }
     
     public void dump() {
