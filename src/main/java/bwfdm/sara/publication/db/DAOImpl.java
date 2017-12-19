49f961c199e22f91f0feaee35295c01b07e4d72f
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2017-12-19 11:25:40 +0100

    db access rewritten using reflection; WIP: just implemented for ItemDB

---
+++ src/main/java/bwfdm/sara/publication/db/DAOImpl.java
@@ -0,0 +1,72 @@
+package bwfdm.sara.publication.db;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import jersey.repackaged.com.google.common.collect.Lists;
+
+public class DAOImpl implements DAO {
+	
+	public Boolean isModified = false;
+	
+	@Override
+	public void set(String fieldName, Object value) {
+		try {
+			Field field = this.getClass().getDeclaredField(fieldName);
+			field.setAccessible(true);
+			field.set(this, value);
+			isModified = true;
+		}
+		catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public Object get(String fieldName) {
+		//System.out.println(this.getClass().getName());
+		try {
+			Field field = this.getClass().getDeclaredField(fieldName);
+			return field.get(this);
+		}
+		catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	@Override
+	public List<String> getDynamicFieldNames() {
+		try {
+			List<String> fn = Lists.newArrayList();
+			String fn_str;
+			fn.clear();
+			//Field[] fields = this.getClass().getDeclaredFields();
+			Field[] fields = this.getClass().getFields();
+			for (Field f : fields) {
+				fn_str = f.getName();
+				fn.add(fn_str);
+			}
+			return fn;
+		}
+		catch (SecurityException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	@Override
+	public List<String> getPrimaryKey() {
+		return Lists.newArrayList("uuid");
+	}
+	
+	@Override
+	public void dump() {
+		System.out.println(this.getClass().getName());
+		System.out.println("=========================");
+		for (String s : getDynamicFieldNames()) {
+			System.out.println( s + "==" + get(s));
+		}
+		
+	}
+}
