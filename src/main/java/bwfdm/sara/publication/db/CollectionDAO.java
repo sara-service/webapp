7fd9076fa05950aafbf925ada02407293a963df4
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2018-01-16 14:21:13 +0100

    add missing display_name field

--- src/main/java/bwfdm/sara/publication/db/CollectionDAO.java
+++ src/main/java/bwfdm/sara/publication/db/CollectionDAO.java
@@ -13,19 +13,21 @@ import jersey.repackaged.com.google.common.collect.Lists;
 public class CollectionDAO extends DAOImpl {
 	public final UUID id;
 	public final String foreign_uuid;
+	public final String display_name;
 	public Boolean enabled;
 
 	public static String TABLE = "Collection";
-	public static List<String> FIELDS = Arrays.asList("id", "foreign_uuid", "enabled");
+	public static List<String> FIELDS = Arrays.asList("id", "foreign_uuid", "display_name", "enabled");
 
 	public CollectionDAO() {
 		id = null;
 		foreign_uuid = null;
-		this.enabled = null;
+		display_name = null;
+		enabled = null;
 	}
 
 	@Override
 	public List<String> getPrimaryKey() {
-		return Lists.newArrayList("id", "foreign_uuid");
+		return Lists.newArrayList("id", "display_name", "foreign_uuid");
 	}
 }
