1d9c4f66d32861bb4f3269a0bf5f37eb96442079
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2018-01-16 11:35:53 +0100

    gnaaa ... make use of uptodate scheme

--- c-initdb.sh
+++ c-initdb.sh
@@ -2,4 +2,7 @@
 
 # initialize postgresql from within eclipse terminal
 # this will run inside container...
+cat saradb/crypto.sql saradb/schema.sql saradb/config.sql \
+        schema.sql licenses.sql permissions.sql >temp.sql
+
 ./udocker.py --repo=/udocker run  -v $PWD/temp.sql:/init.sql  -v $PWD/postgresql.sh:/init.sh -i -t --rm --user postgres  c1t4r/sara-server-vre   /init.sh
