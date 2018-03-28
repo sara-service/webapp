67d0b8dde766cdc82ac7fb009fddba0174e4e21e
Matthias Fratz <matthias.fratz@uni-konstanz.de>
2018-03-28 20:39:02 +0200

    fix archive gitlab auth failure

--- src/main/java/bwfdm/sara/git/gitlab/PrivateTokenREST.java
+++ src/main/java/bwfdm/sara/git/gitlab/PrivateTokenREST.java
@@ -1,11 +1,16 @@
 package bwfdm.sara.git.gitlab;
 
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
 import bwfdm.sara.auth.AuthenticatedREST;
 
 /** REST helper with GitLab {@code PRIVATE-TOKEN: t0k3N} authentication. */
 class PrivateTokenREST extends AuthenticatedREST {
 	PrivateTokenREST(final String root, final String token) {
 		super(root);
-		addHeader("PRIVATE-TOKEN", token);
+		MultiValueMap<String, String> auth = new LinkedMultiValueMap<>();
+		auth.add("PRIVATE-TOKEN", token);
+		setAuth(auth);
 	}
 }
