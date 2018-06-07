3e6eda26272170d1a9fe6ccea38c76d0a75c4f97
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2018-06-07 12:41:36 +0200

    converting linefeeds

--- src/main/java/bwfdm/sara/publication/dspace/DSpace_v6.java
+++ src/main/java/bwfdm/sara/publication/dspace/DSpace_v6.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 //import javax.ws.rs.core.Cookie;
