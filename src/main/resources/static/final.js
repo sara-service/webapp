898f7dd19b6f45f8117f705fe909379b27aa742f
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2018-06-11 13:40:55 +0200

    add final summary page js

---
+++ src/main/resources/static/final.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var has_block = { meta: false };
+
+function blockLoaded(name) {
+	has_block[name] = true;
+	if (Object.values(has_block).indexOf(false) >= 0)
+		return;
+
+	$("#loading").remove();
+	$("#content").removeClass("hidden");
+	$("#next_button").click(function() {
+		// FIXME ?id=deadbeef
+		location.href = "/api/publish/trigger";
+	});
+	$("#next_button").removeClass("disabled");
+}
+
+function initMeta(info) {
+	$.each(["title", "description", "version", "pubrepo_displayname", "collection_displayname",
+		"email"], function(_, name) {
+			$("#" + name).text(info[name]);
+		});
+
+	$('#pubrepo_displayname').prop('title',info["pubrepo"]);
+	$('#collection_displayname').prop('title',info["collection"]);
+	blockLoaded("meta");
+}
+
+
+$(function() {
+	API.get("load metadata fields", "/api/publish/meta", {}, initMeta);
+});
