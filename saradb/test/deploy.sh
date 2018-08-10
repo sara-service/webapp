b9039890e4158b377ee517d0a9062beaf8ba6d83
Matthias Fratz <matthias.fratz@uni-konstanz.de>
2018-08-10 13:43:36 +0200

    force postgresql port on deploy
    if postgresql picks a different port, everything will seem to work but
    the webapp crashes with an error message. with the port forced to 5432,
    the deploy fails if something goes wrong, avoiding a silent failure.

--- saradb/test/deploy.sh
+++ saradb/test/deploy.sh
@@ -6,12 +6,12 @@
 
 case "$SSH_ORIGINAL_COMMAND" in
 tomcat)
-	sudo rm -rf /var/lib/tomcat8/work/Catalina/localhost/SaraServer/ </dev/null
+	sudo rm -rf /var/lib/tomcat8/webapps/SaraServer/ </dev/null
 	sudo dd of=/var/lib/tomcat8/webapps/SaraServer.war
 	sudo service tomcat8 restart </dev/null
 	;;
 postgres)
-	sudo pg_dropcluster --stop 10 main </dev/null || true
+	sudo pg_dropcluster -p 5432 --stop 10 main </dev/null || true
 	sudo pg_createcluster --start 10 main </dev/null
 	sudo -u postgres -H createuser -l -D -R -S sara </dev/null
 	sudo -u postgres -H createdb -E UTF8 -O sara sara </dev/null
