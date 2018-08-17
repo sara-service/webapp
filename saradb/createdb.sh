ecdedd131c999230352650d8583b6e6fcdd01b84
Matthias Fratz <matthias.fratz@uni-konstanz.de>
2018-08-17 14:58:46 +0200

    make postgres database init actually stop on errors
    default behavior, unfortunately, is ON ERROR RESUME NEXT, leaving a
    half-baked database with some tables missing if there are any errors in
    the schema.

--- saradb/createdb.sh
+++ saradb/createdb.sh
@@ -8,12 +8,12 @@ ENV=$3
 createuser -l -D -R -S $USER
 createdb -E UTF8 -O $USER $DB
 
-psql -d $DB -f ./adminconfig.sql
-psql -d $DB -f ./schema.sql
-sed "s/__USERNAME__/$USER/g" permissions.sql | psql -d $DB
+psql -v ON_ERROR_STOP=on -d $DB -f ./adminconfig.sql
+psql -v ON_ERROR_STOP=on -d $DB -f ./schema.sql
+sed "s/__USERNAME__/$USER/g" permissions.sql | psql -v ON_ERROR_STOP=on -d $DB
 # FIXME should just run the license updater instead!
-psql -d $DB -f ./licenses.sql
+psql -v ON_ERROR_STOP=on -d $DB -f ./licenses.sql
 
 for file in "$BASEDIR/$ENV"/*.sql; do
-	psql -d $DB -v "basedir=$BASEDIR/$ENV" -f "$file"
+	psql -v ON_ERROR_STOP=on -d $DB -v "basedir=$BASEDIR/$ENV" -f "$file"
 done
