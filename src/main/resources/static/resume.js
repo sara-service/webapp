2861cb807fd640c04da9cac4511fb006abcc57cf
Matthias Fratz <matthias.fratz@uni-konstanz.de>
2018-08-17 18:20:28 +0200

    restore "resume publication" page, now with search

--- src/main/resources/static/resume.js
+++ src/main/resources/static/resume.js
@@ -4,18 +4,28 @@ function addProjectButton(project) {
 	var form = template("template");
 	form.title.text(project.title);
 	form.version.text(project.version);
-	form.select.attr("href", URI("/done.html").search({ item: project.item }));
-	$("#projects").append(form.root);
+	form.root.attr("href", URI("/info.html").search({ item: project.item }));
+	form.root.insertBefore($("#overflow"));
+	return form.root;
+}
+
+function initProjects(projects) {
+	if (projects.length > 0) {
+		var items = [];
+		$.each(projects, function(_, project) {
+			items.push(addProjectButton(project));
+		});
+		initSearch($('#search'), items, $('#overflow'));
+
+		$("#projects").removeClass("hidden");
+		$("#search_block").removeClass("hidden");
+		// pre-focus the box so the user just has to start typing
+		$('#search').focus();
+	} else
+		$("#noprojects").removeClass("hidden");
+	$("#loading").remove();
 }
 
 $(function() {
-	API.get("load list of projects", "/api/publish/list", {}, function(list) {
-		if (list.length === 0)
-			$("#noprojects").removeAttr("style")
-		else
-			$.each(list, function(_, project) {
-				addProjectButton(project);
-			});
-		$("#loading").remove();	
-	});
+	API.get("load list of projects", "/api/publish/list", {}, initProjects);
 });
