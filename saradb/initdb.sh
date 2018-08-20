3b4d763e5c5f5879e55514e6f69116be82dc80a0
Matthias Fratz <matthias.fratz@uni-konstanz.de>
2018-08-20 14:44:31 +0200

    don't sync database when running locally
    this can cause corruption on crashes, but for an expendable database
    that's fine. it also saves ~30 seconds (~40%) of startup time, though.
    that's quite a significant speedup for the development database!

--- saradb/initdb.sh
+++ saradb/initdb.sh
@@ -4,7 +4,7 @@ cd $BASEDIR
 
 pg_dropcluster --stop 10 main
 service postgresql restart
-pg_createcluster -p 5432 --start 10 main
+pg_createcluster -p 5432 --start 10 main -- --nosync
 
 ./createdb.sh test test devel
 
