7dfb33099e56d6212d58ede6c8565222ad7c9a2d
Matthias Fratz <matthias.fratz@uni-konstanz.de>
2018-09-04 11:07:06 +0200

    deploy shib to test; use eppn instead of persistent-id

--- saradb/test/github-shib.sql
+++ saradb/test/github-shib.sql
@@ -11,20 +11,23 @@ DECLARE sRef UUID;
 
 BEGIN
 
--- GitHub "demo" app, *with* Shib
+-- GitHub "development" app (return address: http://localhost:8080/)
 INSERT INTO source(display_name, contact_email, adapter, url, enabled, logo_url) VALUES
 	('GitHub, Inc.', 'project-sara+github@uni-konstanz.de',
 		'GitHubRESTv3', 'https://github.com', TRUE,
 		'data:image/svg+xml;base64,' || encode(lo_get(logo), 'base64'))
 	RETURNING uuid INTO sRef;
 INSERT INTO source_params(id, param, value) VALUES
-	(sRef, 'oauthID', 'XXX'),
-	(sRef, 'oauthSecret', 'XXX'),
+	(sRef, 'oauthID', 'eed8cfac289b122c7e9e'),
+	(sRef, 'oauthSecret', 'ba37452c50b23f6f11b8f09f28107c46c17c5286'),
 	(sRef, 'shibSurname', 'sn'),
 	(sRef, 'shibGivenName', 'givenName'),
 	(sRef, 'shibEmail', 'mail'),
 	(sRef, 'shibID', 'eppn'),
 	(sRef, 'shibDisplayName', 'displayName'),
 	(sRef, 'nameRegex', 'western');
+
+-- erase the temporary large object
+PERFORM lo_unlink(logo);
 
 END $$;
