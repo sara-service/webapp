17f94212eafbca871bf2707f7c7cf8e9ebd2a38b
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2018-10-25 14:24:07 +0200

    kill all REST code MWHAHAHAHAAA!

--- src/main/java/bwfdm/sara/publication/dspace/dto/v6/BitstreamObject.java
+++
@@ -1,9 +0,0 @@
-package bwfdm.sara.publication.dspace.dto.v6;
-
-/**
- * 
- * @author vk
- */
-public class BitstreamObject {
-
-}
--- src/main/java/bwfdm/sara/publication/dspace/dto/v6/HierarchyCollectionObject.java
+++
@@ -1,8 +0,0 @@
-package bwfdm.sara.publication.dspace.dto.v6;
-
-public class HierarchyCollectionObject {
-	
-	public String id;
-	public String name;
-	public String handle;
-}
