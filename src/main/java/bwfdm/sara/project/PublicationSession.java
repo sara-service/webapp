224c4e622f81f314578a0ad04d928bac455e3b1d
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2019-01-23 10:17:34 +0100

    seed hash using pseudo random ng

--- src/main/java/bwfdm/sara/project/PublicationSession.java
+++ src/main/java/bwfdm/sara/project/PublicationSession.java
@@ -1,8 +1,8 @@
 package bwfdm.sara.project;
 
-import java.util.Calendar;
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
@@ -42,7 +42,8 @@ public class PublicationSession {
 		this.config = config;
 		this.verificationCode = getVerificationCode();
 		this.isVerified = false;
-		seed = String.valueOf(Calendar.getInstance().getTimeInMillis());
+		Random r = new Random();
+		seed = String.valueOf(String.valueOf(r.nextLong()));
 	}
 
 	public UUID getSourceUUID() {
