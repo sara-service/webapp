764677847995b0d334db7a0b9b5e600425c3fc1b
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2019-02-08 10:28:49 +0100

    preparations to unite final_{workspace,workflow}, adding a link to the IR in case of workflow

--- src/main/resources/static/final.js
+++ src/main/resources/static/final.js
@@ -8,7 +8,9 @@ function initRepoInfo(list) {
 			$('#contact_mail').text(r.contact_email);
 			$('#contact_mail').attr('href', "mailto:"+r.contact_email+"?subject=SARA%20Service");
 			$('#help_text').text(r.help);
-			$('#next_button').text("log in to " + r.display_name + " and complete submission →");
+			$('#next_button_workspace').text("log in to " + r.display_name + " and complete submission →");
+			$('#next_button_workflow').text("visit " + r.display_name + " and browse latest publications →");
+			$('#next_button_workflow').attr('href',r.url);
 			return false;
 		}
 	});
@@ -19,8 +21,9 @@ function initMeta(info) {
 
 	API.get("retrieve repo info", "/api/pubrepo-list", {}, initRepoInfo );
 	
-	$('#next_button').attr('href',info["repository_url"]);
-	$('#next_button').removeClass("disabled");
+	$('#next_button_workspace').attr('href',info["repository_url"]);
+	$('#next_button_workspace').removeClass("disabled");
+	$('#next_button_workflow').removeClass("disabled");
 	
 	$("#loading").remove();
 	$("#content").removeClass("hidden");
